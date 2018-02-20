f76a0663f656ea3efbd4858ea335e5ae3f82cbea
Pedro Pinheiro <pedro.pinheiro@mturgov.br>
2018-02-20 14:51:50 -0300

    primeiro commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name totalAutoCenterApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the totalAutoCenterApp
+ */
+angular.module('totalAutoCenterApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name totalAutoCenterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the totalAutoCenterApp
+ */
+angular.module('totalAutoCenterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
